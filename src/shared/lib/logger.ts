c370f94f1adf275e5cb919987cbbcfef58491b9d
vyachean <vyachean@gmail.com>
2024-09-30 13:39:08 +0400

    update styles for logger

--- src/shared/lib/logger.ts
+++ src/shared/lib/logger.ts
@@ -8,6 +8,19 @@ const stringToHue = (str: string): number => {
   return hash % 360;
 };
 
+const colorStrings = (...strList: string[]): string[] => {
+  const style = strList.reduce<string[]>((pre, str) => {
+    const { darkColor, lightColor } = getContrastingColorsFromHSL(str);
+    pre.push(
+      `color:${darkColor};background-color:${lightColor};padding:2px;border:1px solid ${darkColor};border-radius:5px;`,
+    );
+    pre.push('');
+    return pre;
+  }, []);
+
+  return [strList.map((s) => `%c${s}`).join('%c '), ...style];
+};
+
 const getContrastingColorsFromHSL = (
   str?: string,
 ): {
@@ -22,17 +35,14 @@ const getContrastingColorsFromHSL = (
 };
 
 export const createLogModule = (moduleName: string) => {
-  const { darkColor, lightColor } = getContrastingColorsFromHSL(moduleName);
-  const styles = `color:${darkColor};background-color:${lightColor};padding:2px;border:1px solid ${darkColor};border-radius:5px;`;
-
   const log = (message: string, ...args: unknown[]) => {
     // eslint-disable-next-line no-console -- for logger
-    console.log(`%c${moduleName}: ${message}`, styles, ...args);
+    console.log(...colorStrings(moduleName, message), ...args);
   };
 
   const debug = (message: string, ...args: unknown[]) => {
     // eslint-disable-next-line no-console -- for logger
-    console.debug(`%c${moduleName}: ${message}`, styles, ...args);
+    console.debug(...colorStrings(moduleName, message), ...args);
   };
 
   return { log, debug };
