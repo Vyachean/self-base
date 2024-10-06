5e61138612a5b33eb27eb99384ad9df6d20cc7c9
vyachean <vyachean@gmail.com>
2024-10-06 13:51:20 +0400

    ignore temp files

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,10 @@ import { setupApp } from './app/setupApp';
 setupApp().mount('#app');
 
 if (import.meta.env.DEV) {
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment -- for only dev
+  /* @ts-nocheck */
   void import('./temp').then(({ start }) => {
     start();
   });
+  /* @ts-check */
 }
